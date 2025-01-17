ebf1b06e2f9adcca0e3685a75f517e36b7afe36f
Kashif Shaikh <kashifshaikh2.0@gmail.com>
2025-01-17 12:16:04 -0500

    updating

---
+++ prompts/format_prompts.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+
+import sys
+
+args = sys.argv[1:]
+
+if len(args) != 2:
+    print("Invalid args")
+    print("usage:", sys.argv[0], "<input file>", "<output file>")
+    sys.exit(1)
+
+
+filename = args[0]
+output = args[1]
+
+if filename == output:
+    print("Invalid args: input file and output file are the same")
+    sys.exit(1)
+
+
+with open(filename) as infile:
+    with open(output, "w") as outfile:
+        while line := infile.readline():
+            line = line.rstrip()
+            if line == "":
+                continue
+            outfile.write(f"positive:{line}\n")
+            outfile.write("negative:\n")
+            outfile.write("----\n")
